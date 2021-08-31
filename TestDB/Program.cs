0a115267b4cd9d22189825d8f681a24f78f182ba
karamovavv <karamovavv>
2021-09-01 00:23:09 +0500

    homework

---
+++ TestDB/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WorkWithDB.CreateUsersTable();
+            WorkWithDB.CreateCategoriesTable();
+            WorkWithDB.CreateAnnouncementTable();
+
+            WorkWithDB.InsertData();
+
+            var users = WorkWithDB.GetData("select * from users");
+            WriteText("Users", users.Item1, users.Item2);
+
+            var categories = WorkWithDB.GetData("select * from categories");
+            WriteText("Categories", categories.Item1, categories.Item2);
+
+            var announcement = WorkWithDB.GetData("select * from announcement");
+            WriteText("Announcement", announcement.Item1, announcement.Item2);
+
+            if (Console.ReadLine().ToLower() == "insert")
+            {
+                Console.WriteLine("В какую таблицу писать?");
+                string table = Console.ReadLine().ToLower();
+                switch (table)
+                {
+                    case "users":
+                        Console.WriteLine("Введите логин пользователя");
+                        string username = Console.ReadLine();
+                        Console.WriteLine("Введите имя пользователя");
+                        string first_name = Console.ReadLine();
+                        Console.WriteLine("Введите фамилию пользователя");
+                        string last_name = Console.ReadLine();
+                        Console.WriteLine("Введите электронный адрес пользователя");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Введите телефон пользователя");
+                        string phone = Console.ReadLine();
+                        WorkWithDB.GetData("INSERT INTO USERS(username, first_name, last_name, email, phone) VALUES('" + username + "', '" + first_name + "', '" + last_name + "', '" + email + "', '" + phone + "')");
+
+                        Console.WriteLine("Пользователь успешно добавлен!");
+                        var users1 = WorkWithDB.GetData("select * from users");
+                        WriteText("Users", users1.Item1, users1.Item2);
+
+                        break;
+                    case "categories":
+                        Console.WriteLine("Введите название категории");
+                        string name_cat = Console.ReadLine();
+                        Console.WriteLine("Введенная категория является верхнеуровневой? (y/n)");
+                        string sub_cat = Console.ReadLine().ToLower();
+                        bool err = false;
+                        if (sub_cat == "y")
+                            WorkWithDB.GetData("INSERT INTO CATEGORIES(NAME) VALUES('" + name_cat + "')");
+                        else
+                        {
+                            Console.WriteLine("Введите название категории, к которой относится добавляемая категория");
+                            string sub_cat_name = Console.ReadLine().ToLower();
+                            var data_cat = WorkWithDB.GetData("select id from CATEGORIES where name like '" + sub_cat_name + "')");
+                            try
+                            {
+                                string id = data_cat.Item2[0][0];
+                                WorkWithDB.GetData("INSERT INTO CATEGORIES(NAME, Main_category) VALUES('" + name_cat + "', " + id + ")");
+                            }
+                            catch
+                            {
+                                err = true;
+                                Console.WriteLine("Не удалось найти заданную категорию.");
+                            }
+                        }
+                        if (!err)
+                        {
+                            Console.WriteLine("Категория успешно добавлена!");
+                            var categories1 = WorkWithDB.GetData("select * from CATEGORIES");
+                            WriteText("Users", categories1.Item1, categories1.Item2);
+                        }
+                        break;
+                    case "announcement":
+                        bool errAnn = false;
+                        Console.WriteLine("Введите заголовок объявления");
+                        string caption = Console.ReadLine();
+                        Console.WriteLine("Введите название категории пользователя");
+                        string category_id = Console.ReadLine().ToLower();
+                        var data = WorkWithDB.GetData("select id from CATEGORIES where name like '" + category_id + "')");
+                        try
+                        {
+                            category_id = data.Item2[0][0];
+                        }
+                        catch
+                        {
+                            errAnn = true;
+                            Console.WriteLine("Не удалось найти заданную категорию");
+                        }
+                        if (!errAnn)
+                        {
+                            Console.WriteLine("Введите логин пользователя");
+                            string user_id = Console.ReadLine();
+                            var data2 = WorkWithDB.GetData("select id from users where name like '" + user_id + "')");
+                            try
+                            {
+                                user_id = data2.Item2[0][0];
+                            }
+                            catch
+                            {
+                                errAnn = true;
+                                Console.WriteLine("Не удалось найти указанного пользователя");
+                            }
+                            if (!errAnn)
+                            {
+                                Console.WriteLine("Введите адрес");
+                                string address = Console.ReadLine();
+                                Console.WriteLine("Введите цену товара");
+                                string price = Console.ReadLine();
+                                Console.WriteLine("Введите описание объявления");
+                                string description = Console.ReadLine();
+                                WorkWithDB.GetData(@"INSERT (caption, category_id, address, price, create_date, user_id, description)
+                                                     VALUES('" + caption + "', " + category_id + ", '" + address + "', '" + price.Replace(" ", "") + "', current_timestamp, " + user_id + ", '" + description + "')");
+                                Console.WriteLine("Объявление успешно добавлено!");
+                                var announcement1 = WorkWithDB.GetData("select * from users");
+                                WriteText("Announcement", announcement1.Item1, announcement1.Item2);
+                            }
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("К сожалению, это невозможно, таблица с заданным именем не найдена.");
+                        break;
+                }
+            }
+            else Environment.Exit(0);
+        }
+
+        private static void WriteText(string nameTable, List<string> headers, List<List<string>> data)
+        {
+            Console.WriteLine($"{nameTable}:\n");
+            string header = "";
+            for (int i = 0; i < headers.Count; i++)
+            {
+                header += headers[i] + " | ";
+            }
+            header.TrimEnd('|');
+            Console.WriteLine(header);
+            string row;
+            for (int i = 0; i < data.Count; i++)
+            {
+                row = "";
+                for (int j = 0; j < data[i].Count; j++)
+                {
+                    row += data[i][j] + " | ";
+                }
+                Console.WriteLine(row);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ TestDB/WorkWithDB.cs
@@ -0,0 +1,245 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDB
+{
+    class WorkWithDB
+    {
+        public const string connectionString = "Host=****;Username=postgres;Password=****;Database=testvally";
+
+        public static void CreateUsersTable()
+        {
+            using (var connection = new NpgsqlConnection(connectionString)) {
+                connection.Open();
+
+                if (!tableIsExest(connection, "users"))
+                {
+                    var sql = @"
+                    CREATE SEQUENCE users_id_seq;
+
+                    CREATE TABLE users
+                    (
+                        id              BIGINT                      NOT NULL    DEFAULT NEXTVAL('users_id_seq'),
+                        username        CHARACTER VARYING(255)      NOT NULL,
+                        first_name      CHARACTER VARYING(255)      NOT NULL,
+                        last_name       CHARACTER VARYING(255),
+                        email           CHARACTER VARYING(255)      NOT NULL,
+                        phone           CHARACTER VARYING(255),
+  
+                        CONSTRAINT users_key PRIMARY KEY (id),
+                        CONSTRAINT users_username_unique UNIQUE (username),
+                        CONSTRAINT users_email_unique UNIQUE (email)
+                    );
+
+                    CREATE UNIQUE INDEX users_email_unq_idx ON users(lower(email));
+                    ";
+
+                    using (var cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public static void CreateAnnouncementTable()
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                if (!tableIsExest(connection, "announcement"))
+                {
+                    var sql = @"
+                    CREATE SEQUENCE announcement_id_seq;
+
+                    select exists (select * from information_schema.tables where table_name = 'table_name' and table_schema = 'public')
+
+                    CREATE TABLE announcement
+                    (
+                        id              BIGINT                      NOT NULL    DEFAULT NEXTVAL('announcement_id_seq'),
+                        caption         CHARACTER VARYING(255)      NOT NULL,
+                        category_id     BIGINT                      NOT NULL,
+                        address         CHARACTER VARYING(255)      NOT NULL,
+                        price           MONEY,      
+                        create_date     TIMESTAMP WITH TIME ZONE    NOT NULL,
+                        user_id         BIGINT                      NOT NULL,
+                        description     CHARACTER VARYING(8000)      NOT NULL,
+  
+                        CONSTRAINT announcement_key PRIMARY KEY (id),
+                        CONSTRAINT announcement_fk_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                        CONSTRAINT announcement_fk_category_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+                    );";
+
+                    using (var cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public static void CreateCategoriesTable()
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                if (!tableIsExest(connection, "categories"))
+                {
+                    var sql = @"
+                    CREATE SEQUENCE categories_id_seq;
+
+                    CREATE TABLE categories
+                    (
+                        id              BIGINT                      NOT NULL    DEFAULT NEXTVAL('categories_id_seq'),
+                        name            CHARACTER VARYING(255)      NOT NULL,
+                        main_category   BIGINT,
+  
+                        CONSTRAINT categories_key PRIMARY KEY (id),
+                        CONSTRAINT categories_fk_category_id FOREIGN KEY (main_category) REFERENCES categories(id) ON DELETE CASCADE
+                    );";
+
+                    using (var cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public static void InsertData()
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var sql1 = @"
+                    TRUNCATE public.users CASCADE;
+                    ALTER SEQUENCE users_id_seq RESTART WITH 1;
+
+                    INSERT INTO USERS (username, first_name, last_name, email, phone)
+                    VALUES ('Ren@r', 'Ренар', null, 'renar1986@mail.ru', '89938848848');
+
+                    INSERT INTO USERS (username, first_name, last_name, email, phone)
+                    VALUES ('Kefir', 'Антон', null, 'my_email111@mail.ru', '8922284888');
+
+                    INSERT INTO USERS (username, first_name, last_name, email, phone)
+                    VALUES ('RoboKop', 'Альберт', 'Никантин', 'nikantin_a@ya.ru', '8991231232');
+
+                    INSERT INTO USERS (username, first_name, last_name, email, phone)
+                    VALUES ('Konstantin', 'Костя', 'Максимов', 'kk_maximov@gmail.com', null);
+
+                    INSERT INTO USERS (username, first_name, last_name, email, phone)
+                    VALUES ('VarVar', 'Варвара', null, 'helentyeva@mail.ru', null);
+                ";
+
+                using (var cmd = new NpgsqlCommand(sql1, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                var sql2 = @"
+                    TRUNCATE public.CATEGORIES CASCADE;
+                    ALTER SEQUENCE categories_id_seq RESTART WITH 1;
+    
+                    INSERT INTO CATEGORIES (ID, NAME, MAIN_CATEGORY)
+                    VALUES (1, 'Транспорт', null);
+
+                    INSERT INTO CATEGORIES (ID, NAME, MAIN_CATEGORY)
+                    VALUES (2, 'Автомобили', 1);
+
+                    INSERT INTO CATEGORIES (ID, NAME, MAIN_CATEGORY)
+                    VALUES (3, 'Мотоциклы', 1);
+
+                    INSERT INTO CATEGORIES (ID, NAME, MAIN_CATEGORY)
+                    VALUES (4, 'Недвижимость', null);
+
+                    INSERT INTO CATEGORIES (ID, NAME, MAIN_CATEGORY)
+                    VALUES (5, 'Квартиры', 4);
+                ";
+
+                using (var cmd = new NpgsqlCommand(sql2, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                var sql3 = @"
+                    TRUNCATE public.ANNOUNCEMENT CASCADE;
+                    INSERT INTO ANNOUNCEMENT (caption, category_id, address, price, create_date, user_id, description)
+                    VALUES ('Hyundai Solaris, 2013', 2, 'г. Уфа', 550000, current_timestamp, 1, 'Автомобиль в хорошем состоянии');
+
+                    INSERT INTO ANNOUNCEMENT (caption, category_id, address, price, create_date, user_id, description)
+                    VALUES ('Квадроцикл Tiger Sport 250 cc', 3, 'г. Уфа', 199900, current_timestamp, 2, 'Ознакомиться со всеми предложениями можно на нашем официальном сайте или в нашем магазине.');
+
+                    INSERT INTO ANNOUNCEMENT (caption, category_id, address, price, create_date, user_id, description)
+                    VALUES ('Снегоход sharmax SN-550 maх PRO', 1, 'г. Уфа', 339900, current_timestamp, 3, 'СНЕГОХОД SHARMAX SN-550 MAХ PRO -  построен на базе инновационной модульной рамы, каждый элемент которой индивидуально вырезается на лазерном станке.');
+
+                    INSERT INTO ANNOUNCEMENT (caption, category_id, address, price, create_date, user_id, description)
+                    VALUES ('1-к. квартира, 28,8 м², 7/28 эт.', 5, 'г. Уфа, Малая Тихорецкая ул.', 2880000, current_timestamp, 4, 'Продам шикарную квартира в ЖК Июнь!');
+
+                    INSERT INTO ANNOUNCEMENT (caption, category_id, address, price, create_date, user_id, description)
+                    VALUES ('2-к. квартира, 76,8 м², 6/16 эт.', 5, 'г. Уфа, ул. Софьи Перовской, 54', 7300000, current_timestamp, 5, 'Продам просторную 2-ком. квартиру в элитном районе Южный.');
+                ";
+                using (var cmd = new NpgsqlCommand(sql3, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+        public static (List<string>, List<List<string>>) GetData(string query)
+        {
+            List<string> headers = new List<string>();
+            List<List<string>> datas = new List<List<string>>();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                var sql = query;
+                NpgsqlCommand command = connection.CreateCommand();
+                command.CommandText = sql;
+                NpgsqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    int countColumn = reader.FieldCount;
+                    for (int i = 0; i < countColumn; i++)
+                        headers.Add(reader.GetName(i));
+                    while (reader.Read())
+                    {
+                        List<string> tmp = new List<string>();
+                        for (int i = 0; i < countColumn; i++)
+                        {
+                            string tmpValue = reader.GetValue(i).ToString();
+                            tmp.Add(tmpValue);
+                        }
+                        datas.Add(tmp);
+                    }
+                }
+                reader.Close();
+                command.Dispose();
+            }
+            return (headers, datas);
+        }
+
+        private static bool tableIsExest(NpgsqlConnection connection, string nameTable)
+        {
+            var sqlCheck = "select exists(select * from information_schema.tables where table_name = '"+ nameTable + "' and table_schema = 'public')";
+            NpgsqlCommand command = connection.CreateCommand();
+            command.CommandText = sqlCheck;
+            NpgsqlDataReader reader = command.ExecuteReader();
+            bool existTable = false;
+            if (reader.HasRows)
+            {
+                if(reader.Read())
+                    existTable = bool.Parse(reader.GetValue(0).ToString());
+            }
+            reader.Close();
+            command.Dispose();
+            return existTable;
+        }
+    }
+}
