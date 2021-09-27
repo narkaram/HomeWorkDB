36a3e871fcfd882614518e341cc65f0cd065e2d9
karamovavv <karamovavv>
2021-09-27 21:09:18 +0500

    hw delegates

---
+++ HomeWork_Delegates/FileWatcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Delegates
+{
+    class FileWatcher
+    {
+        public string directory { get; set; }
+
+        public event EventHandler<FileArgs> FileFound;
+
+        public FileWatcher(string dir)
+        {
+            directory = dir;
+        }
+
+        public void Search(string searchPattern)
+        {
+            foreach (var file in Directory.GetFiles(directory, searchPattern))
+            {
+                var args = new FileArgs(file);
+                FileFound?.Invoke(this, args);
+                if (args.CancelRequested)
+                    break;
+            }
+        }
+    }
+    public class FileArgs : EventArgs
+    {
+        public string NameFile { get; }
+        public bool CancelRequested { get; set; }
+
+        public FileArgs(string fileName)
+        {
+            NameFile = fileName;
+        }
+    }
+}
---
+++ HomeWork_Delegates/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HomeWork_Delegates
+{
+    class Program
+    {
+        public event EventHandler<FileArgs> FileFound;
+        static void Main(string[] args)
+        {
+            //////1
+            Test1[] mas = { new Test1(1), new Test1(2), new Test1(15), new Test1(3), new Test1(4), new Test1(5) };
+            var b = ExtensionClass.GetMax<Test1>(mas, toFloat);
+            Console.WriteLine("Максимальное число - " + b.a.ToString());
+            Console.WriteLine("");
+            //////2
+            FileWatcher fileWatcher = new(@"D:\Рабочий стол\ДЗ"); 
+            fileWatcher.FileFound += onFileFound;
+            fileWatcher.Search("");
+        }
+
+        public static void onFileFound(object sender, FileArgs eventArgs)
+        {
+            Console.WriteLine(eventArgs.NameFile);
+            if (Path.GetFileName(eventArgs.NameFile) == "exit.txt")
+                eventArgs.CancelRequested = true;
+        }
+
+        static public float toFloat(Test1 a)
+        {
+            return (float)a.a;
+        }
+
+    }
+
+    #region первое задание
+    class Test1
+    {
+        public int a { get; set; }
+        public Test1()
+        {
+            a = 15;
+        }
+        public Test1(int _a)
+        {
+            a = _a;
+        }
+
+    }
+    public static class ExtensionClass //where T : class
+    {
+        public static T GetMax<T>(this IEnumerable<T> e, Func<T, float> getParameter)
+        {
+            var digits = e.Select(getParameter);
+            var max = digits.Max();
+            var res = e.First(item => getParameter(item) == max);
+            return res;
+        }
+    }
+    #endregion
+
+}
